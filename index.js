c780e7268c6145f9e9bc529195fd4f6362c10afd
Dolan Miu <dolan_miu@hotmail.com>
2019-05-22 01:22:19 +0100

    Turn it into an express app

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: true, typeInterval: 1 })
+const Nightmare = require("nightmare");
+const nightmare = Nightmare({ show: false, typeInterval: 1 });
+const express = require("express");
 
-nightmare
-  .goto('https://www.blackrock.com/tools/pretrade')
-  .wait('#signOnBean')
-  .type('#userName', '')
-  .type('#password', '')
-  .click('#submitLogin')
-  .wait('.application')
-  .cookies.get()
-  .end()
-  .then((cookies) => {
-    const cookie = cookies.find(c => c.name === 'JSESSION_blk-tools02');
-    console.log(cookie);
-  })
-  .catch(error => {
-    console.error('Search failed:', error)
-  })
+const app = express();
+
+app.get("/", (req, res) =>
+  nightmare
+    .goto("https://www.blackrock.com/tools/pretrade")
+    .wait("#signOnBean")
+    .type("#userName", req.query.username)
+    .type("#password", req.query.password)
+    .click("#submitLogin")
+    .wait(".application")
+    .cookies.get()
+    .end()
+    .then(cookies => {
+      const cookie = cookies.find(c => c.name === "JSESSION_blk-tools02");
+      console.log(cookie);
+      res.status(200).send(cookie);
+    })
+    .catch(error => {
+      console.error("Search failed:", error);
+      res.status(500).send(error);
+    })
+);
+
+app.listen(3001);
